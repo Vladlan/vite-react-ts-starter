0ff6851a9c0f2fe8169178a06502dbbeca672724
Vladlan <lanvlad911@gmail.com>
2023-06-12 10:42:48 +0200

    Add vite-react-ts-starter

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import checker from 'vite-plugin-checker';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    checker({
+      typescript: true,
+    }),
+  ],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./src/setupTests.ts'],
+  },
+});
